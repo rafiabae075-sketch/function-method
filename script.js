7fbf6318103ebae01b872caa7a2a9f1622cc0c67
rafia bae <rafiabae36@gmail.com>
2025-10-16 13:43:21 -0700

    function
    code done

---
+++ script.js
@@ -0,0 +1,39 @@
+function myFunction() {
+    console.log("nexskill");
+    console.log("we are learning js");
+    console.log("welcom to vs code");
+}
+myFunction();
+myFunction();
+
+
+let x1=5
+let y1=7
+function sum(x,y) {
+    console.log(x+y);
+}
+ sum(x,y)
+function sum(x,y){
+   s=x+y ;
+   return s;
+}
+let x=5
+let y=3
+
+// let value = sum(3,5)
+console.log(x+y);
+
+const mulArrow= (a,b)=> {
+ console.log(a*b);
+ return a*b
+}
+ let arr = [1,2,3,4,5];
+ arr.forEach((val,idx,arr)=> {
+    console.log(val,idx,arr);
+ });
+let num = [1,2,3,4,5,6]
+num.forEach((num)=>{
+    console.log(num*num);
+    
+}
+)
